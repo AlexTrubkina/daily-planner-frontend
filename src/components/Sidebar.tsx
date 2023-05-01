97bf2c904163903840365b4cd389e12c48d2cc59
AlexTrubkina <sashatru99@gmail.com>
2023-05-01 21:26:51 +0300

    add icons to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Heading, Stack } from "@chakra-ui/react";
+import { Box, Heading, Stack, Icon, Flex } from "@chakra-ui/react";
 
 import { sidebarLinks } from "../data/datasets";
 import { Link } from "react-router-dom";
@@ -16,25 +16,39 @@ function Sidebar(): JSX.Element {
             boxShadow="34px 34px 60px #bebebe, -34px -34px 60px #ffffff"
         >
             <Box mb="40px">
-                <Heading color="blackAlpha.700" fontSize="3xl" fontWeight="bold">
-                    Daily Planner
-                </Heading>
+                <Link to="/">
+                    <Heading
+                        color="blackAlpha.700"
+                        fontSize="3xl"
+                        fontWeight="bold"
+                    >
+                        Daily Planner
+                    </Heading>
+                </Link>
             </Box>
             <Stack spacing={4}>
                 {sidebarLinks.map((link) => (
                     <Link key={link.name} to={link.link}>
-                    <Box
-                        fontWeight="semibold"
-                        color="blackAlpha.600"
-                        backgroundColor="linear-gradient(145deg, #f0f0f0, #cacaca)"
-                        boxShadow="9px 9px 18px #bebebe, -9px -9px 18px #ffffff"
-                        _hover={{ background: "linear-gradient(145deg,#cacaca, #f0f0f0)", cursor: "pointer" }}
-                        borderRadius="10px"
-                        padding="10px"
-                        
-                    >
-                        {link.name}
-                    </Box>
+                        <Flex
+                            fontWeight="semibold"
+                            color="blackAlpha.600"
+                            backgroundColor="linear-gradient(145deg, #f0f0f0, #cacaca)"
+                            boxShadow="9px 9px 18px #bebebe, -9px -9px 18px #ffffff"
+                            _hover={{
+                                background:
+                                    "linear-gradient(145deg,#cacaca, #f0f0f0)",
+                                cursor: "pointer",
+                            }}
+                            borderRadius="10px"
+                            padding="10px"
+                            alignItems="center"
+                            gap="10px"
+                        >
+                            <Icon viewBox='0 0 17 15' fontSize="18px">
+                                {link.icon}
+                            </Icon>
+                            {link.name}
+                        </Flex>
                     </Link>
                 ))}
             </Stack>
--- src/data/datasets.tsx
+++ src/data/datasets.tsx
@@ -1,8 +1,55 @@
-export const sidebarLinks: {name: string, link: string}[] = [
-    {"name":"Мой дневник", "link":"/dashboard"},
-    {"name":"Мое настроение", "link":"/dashboard"},
-    {"name":"Профиль", "link":"/dashboard"},
-];
+export const sidebarLinks: { name: string; link: string; icon: JSX.Element }[] =
+    [
+        {
+            name: "Мой дневник",
+            link: "/dashboard",
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-house-door"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+                </svg>
+            ),
+        },
+        {
+            name: "Мое настроение",
+            link: "/dashboard",
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-emoji-smile"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z" />
+                </svg>
+            ),
+        },
+        {
+            name: "Профиль",
+            link: "/dashboard",
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-person"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                </svg>
+            ),
+        },
+    ];
 
 export const navLinks: Array<string | null | undefined> = [
     "О нас",
